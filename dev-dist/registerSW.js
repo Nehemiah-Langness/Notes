be4d3396beb54b57f6feb4e9b0b85d7e8d1fa793
Nehemiah Langness <32148982+Nehemiah-Langness@users.noreply.github.com>
2024-03-07 09:17:04 -0600

    added PWA capabilities

---
+++ dev-dist/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) navigator.serviceWorker.register('/Notes/dev-sw.js?dev-sw', { scope: '/Notes/', type: 'classic' })
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate', devOptions: {
+      enabled: true
+    },
+    includeAssets: ['favicon.svg'],
+    manifest: {
+      name: 'Notes',
+      short_name: 'Notes',
+      description: 'Keep track of things with virtual sticky notes',
+      theme_color: '#fcffcc',
+      background_color: '#dbdbdb',
+      icons: [
+        {
+          src: 'logo192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'logo512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
+  })],
   base: "/Notes/"
 })
