af42ccafd4a33cc312e8370305ae2015c696adc4
Nehemiah Langness <32148982+Nehemiah-Langness@users.noreply.github.com>
2024-03-17 14:30:04 -0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Notes/sw.js', { scope: '/Notes/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
